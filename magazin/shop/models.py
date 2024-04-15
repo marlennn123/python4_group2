96c75439a76645a01b6cd8fe625d249c8030f990
Usman <usmanmamatkulov4.com>
2024-04-16 01:03:33 +0600

    мен буттум, view.py ды жаз

---
+++ magazin/shop/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=32, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    tittle = models.CharField(max_length=32)
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    date = models.DateField()
+    price = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.tittle
+
+
+class Comment(models.Model):
+    text = models.TextField()
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+
+
+    
+
+
+
+
+
